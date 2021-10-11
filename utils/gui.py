ba248f7cbe72112a87421b44fd7a1a666220d4a2
thatcher thornberry <thatcherthornberry@gmail.com>
2021-10-11 16:22:02 -0500

    click_image

--- utils/gui.py
+++ utils/gui.py
@@ -1,6 +1,8 @@
 from Xlib import display, X
 from constants import GAME_WINDOW_HEIGHT, GAME_WINDOW_WIDTH, GAME_WINDOW_X, GAME_WINDOW_Y
 import pyautogui
+import random
+import time
 
 def get_and_move_display():
     """
@@ -71,13 +73,30 @@ def click_image(image):
         coordinates of image
     """
     # find image on screen
-    loc = pyautogui.locationOnScreen(image) #Box type with attributes, left, top, width, height
+    loc = pyautogui.locateOnScreen(image) #Box type with attributes, left, top, width, height
     point = pyautogui.center(loc)
 
+    # icons are 20x20
+
     x, y = point
+
+    #get random int between 0 and 8
+    x = x + random.randint(-8, 8)
+    y = y + random.randint(-8, 8)
 
     pyautogui.click(x, y) # click center of image
 
 if __name__ == "__main__":
     get_and_move_display()
-    save_window_screenshot()
+    click_image('./images/icons/tinderbox.png')
+    click_image('./images/icons/wood.png')
+    time.sleep(5.6)
+    click_image('./images/icons/tinderbox.png')
+    click_image('./images/icons/wood.png')
+    time.sleep(5.5)
+    click_image('./images/icons/tinderbox.png')
+    click_image('./images/icons/wood.png')
+    time.sleep(4.5)
+    click_image('./images/icons/tinderbox.png')
+    click_image('./images/icons/wood.png')
+
